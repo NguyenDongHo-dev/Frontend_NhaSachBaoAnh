bd4ef12d509170beb0bf981f4563a60006bab139
NguyenDongHo-dev <nguyendongho-dev@gmail.com>
2025-09-17 21:05:29 +0700

    update component Navbar admin

--- components/admin/navbar.tsx
+++ components/admin/navbar.tsx
@@ -1,6 +1,18 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathName = usePathname();
+
+  const menus = [
+    { name: "Dashboard", href: "/admin/dashboard" },
+    { name: "Người dùng", href: "/admin/user" },
+    { name: "Sản phẩm", href: "/admin/product" },
+    { name: "Đơn hàng", href: "/admin/orders" },
+  ];
+
   return (
     <div className="min-w-[200px] fixed h-screen top-0 left-0 bottom-0 z-30  py-[15px] px-3 border-r-2 bg-blue-500 text-white ">
       <div className="flex flex-col justify-between h-full">
@@ -9,36 +21,16 @@ export default function Navbar() {
             Trang quan trị
           </h1>
           <div className="flex flex-col gap-2 uppercase font-medium">
-            <Link
-              className="pl-[15px] hover:bg-blue-400 py-2 rounded-[4px]"
-              href={"/admin/dashboard"}
-            >
-              Dashboard
-            </Link>
-            <Link
-              className="pl-[15px] hover:bg-blue-400 py-2 rounded-[4px]"
-              href={"/admin/orders"}
-            >
-              Order
-            </Link>
-            <Link
-              className="pl-[15px] hover:bg-blue-400 py-2 rounded-[4px]"
-              href={"/admin/product"}
-            >
-              Product
-            </Link>
-            <Link
-              className="pl-[15px] hover:bg-blue-400 py-2 rounded-[4px]"
-              href={"/admin/user"}
-            >
-              User
-            </Link>
-            <Link
-              className="pl-[15px] hover:bg-blue-400 py-2 rounded-[4px]"
-              href={"/admin/categories"}
-            >
-              Categories
-            </Link>
+            {menus.map((item) => (
+              <Link
+                className={`pl-[15px] hover:bg-blue-400 py-2 rounded-[4px] ${
+                  pathName.startsWith(item.href) && "bg-blue-400"
+                }`}
+                href={item.href}
+              >
+                {item.name}
+              </Link>
+            ))}
           </div>
         </div>
         <div className="pl-[15px] mb-[20px] cursor-pointer py-2  hover:bg-blue-300 rounded-[4px] uppercase font-bold">
